292924d7b852229a3e9d50217322be33247c9fbd
Replit user <>
2023-08-25 04:21:02 +0000

    new try

--- list.c
+++ list.c
@@ -121,8 +121,10 @@ void * popBack(List * list) {
 // **Nota**: El current debe quedar apuntando al nodo siguiente del eliminado.
 void * popCurrent(List * list) {
   
-  list -> current -> prev -> next = list -> current -> next;
-
+  if(list -> current -> prev != NULL){
+    list -> current -> prev -> next = list -> current -> next;
+  }
+  
   list -> current = list -> current -> next;
   
   return list -> current -> data;
