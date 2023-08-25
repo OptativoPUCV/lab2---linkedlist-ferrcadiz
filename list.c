1b3b0f4a0feac93ae5d23785f5b2e86bb5c420eb
Replit user <>
2023-08-25 04:46:26 +0000

    new try

--- list.c
+++ list.c
@@ -130,6 +130,7 @@ void * popCurrent(List * list) {
 
   if(list -> current -> next != NULL){
     list -> current -> next -> prev = list -> current -> prev;
+    list -> current -> prev -> next = list -> current -> next;
     list -> current = list -> current -> next;
   }
   else{
@@ -137,7 +138,6 @@ void * popCurrent(List * list) {
     list -> current = NULL;
   }
 }
-
 void cleanList(List * list) {
     while (list->head != NULL) {
         popFront(list);
